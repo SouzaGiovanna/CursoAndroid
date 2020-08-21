4458a6982dea5b86b90146becd11820795cc5887
SouzaGiovanna <gi.souza03@gmail.com>
2020-08-21 14:32:36 -0300

    ajustes cores

--- AppMapa/app/src/main/java/com/cursoandroid/appmapa/MainActivity.java
+++ AppMapa/app/src/main/java/com/cursoandroid/appmapa/MainActivity.java
@@ -2,6 +2,7 @@ package com.cursoandroid.appmapa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void exibirMapa(View view) {
-
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
