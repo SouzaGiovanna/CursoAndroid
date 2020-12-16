2d0c54770c13026fe7ef0ab8244fb916698ac023
SouzaGiovanna <gi.souza03@gmail.com>
2020-12-16 11:04:27 -0300

    Finalizando o projeto SeekBar

--- Seekbar/app/src/main/java/com/cursoandroid/seekbar/MainActivity.java
+++ Seekbar/app/src/main/java/com/cursoandroid/seekbar/MainActivity.java
@@ -2,13 +2,45 @@ package com.cursoandroid.seekbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private SeekBar seekBarEscala;
+    private TextView txtResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        seekBarEscala = findViewById(R.id.seekBarEscala);
+        txtResultado = findViewById(R.id.txtResultado);
+
+        seekBarEscala.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                txtResultado.setText("Progresso: " +i+ " / " +seekBar.getMax());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void recuperarProgresso(View view){
+        txtResultado.setText("Escolhido: " +this.seekBarEscala.getProgress());
     }
 }
